fc96e69d2e171c4e1dcbbbf0a1a7b3d117b41780
Ojasva Jain <ojasva.jain@outlook.com>
2016-11-08 23:37:03 +0530

    something

--- Helper Codes/test.cpp
+++ Helper Codes/test.cpp
@@ -7,6 +7,7 @@ int main(){
 	char buf[BUF_SIZE];
 	char ch[1];
 	char *t;
+	struct sockaddr_in sadd,cadd;
 
 	int ls=socket(AF_INET, SOCK_STREAM,0);
 	sadd.sin_family=AF_INET;
@@ -17,14 +18,17 @@ int main(){
 	clen=sizeof(cadd);
 	int sd=accept(ls,(struct sockaddr *)&cadd,&clen);
 
-	columnExtractor("dataset.txt", 3);
+	double *values=NULL;
+	int sizeArray=columnExtractor("dataset.txt", 3,&values);
 	char *number = toArray(123.45);
-	*number=toArray(123);
-	char iplist[WORKERS];getIpList(iplist);
+	number=toArray((long int)123);
+	char *iplist[WORKERS];getIpList(iplist);
 	string part = getPartition("dataset.txt",500,1);
-	int fsize(fopen("dataset.txt","r"));
-	char buf[]={'1','2','3','4','\0'};int itCount = getIterationCount(buf);
+	getFileSize(fopen("dataset.txt","r"));
+	char buf1[]={'1','2','3','4','\0'};int itCount = getIterationCount(buf);
 	sendIterationCount(fopen("dataset.txt","r"),sd,"");
+	
+	FILE *fp=fopen("dataset.txt","r");
 	readSend(fp,sd);
 	
 	return 0;
